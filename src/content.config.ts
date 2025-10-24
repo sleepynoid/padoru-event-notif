30a6822128f540a88867d69104982f7971a2bc89
sleepynoid <aliffarhan003@gmail.com>
2025-10-25 01:02:03 +0700

    add new theme working

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
-});
-
 const eventMetadata = defineCollection({
 	// Load Markdown files in the `src/content/event-metadata/` directory.
 	loader: glob({ base: './src/content/event-metadata', pattern: '**/*.md' }),
@@ -31,4 +16,4 @@ const eventMetadata = defineCollection({
 	}),
 });
 
-export const collections = { blog, eventMetadata };
+export const collections = { eventMetadata };
